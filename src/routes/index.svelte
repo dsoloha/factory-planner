0e9ed2737f2fae9673c373b70937c3f36fd3f2ee
DSoloha <dsoloha@live.com>
2021-08-02 00:16:05 -0400

    Added basic draggable elements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,12 @@
+<script>
+	import Draggable from '../components/draggable.svelte';
+</script>
+
 <main>
 	<h1>Factory Planner</h1>
 	<p>New</p>
 	<p>Open</p>
 	<p>Load</p>
+
+	<Draggable>Drag Me</Draggable>
 </main>
